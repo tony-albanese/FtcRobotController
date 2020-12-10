981caa7ad03067f628f858ddb4425389172b8cd1
tony-albanese <tony.albanese2000@gmail.com>
2020-12-10 08:14:46 +0100

    Added sensor code for these test classes. (Still not correct.)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/KillerRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/KillerRobot.java
@@ -1,11 +1,18 @@
 package org.firstinspires.ftc.teamcode.killer_robot;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorREV2mDistance;
+
+@TeleOp(name = "Killer Robot Base Class", group = "Linear Opmode")
 public class KillerRobot extends LinearOpMode {
+
 
     DcMotor leftDriveOne;
     DcMotor leftDriveTwo;
@@ -18,40 +25,58 @@ public class KillerRobot extends LinearOpMode {
     Servo leftElbow;
     Servo rightElbow;
 
-    private float MOTOR_POWER = 0.5f;
-    public boolean objectIsClose = false;
+    ColorSensor leftColorSensor;
+    ColorSensor rightColorSensor;
 
+    TouchSensor touchSensor;
+
+    SensorREV2mDistance distanceSensor = null;
+
+
+    private double MOTOR_POWER = 0.5;
+    public boolean objectIsClose = false;
 
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        System.out.println("Hello, world!");
         initializeHardware();
         waitForStart();
 
-        while (opModeIsActive() && !objectIsClose){
+        while (opModeIsActive() && !objectIsClose) {
             runKillerRobot();
-            stopKillerRobot();
+
+            if (gamepad1.a || touchSensor.isPressed()) {
+                stopKillerRobot();
+            }
+
         }
 
         resetRobot();
     }
 
 
+    private final void initializeHardware() {
 
-
-
-    private final void initializeHardware(){
+        //Initialize the motors
         leftDriveOne = hardwareMap.get(DcMotor.class, "left_drive_one");
         leftDriveTwo = hardwareMap.get(DcMotor.class, "left_drive_two");
         rightDriveOne = hardwareMap.get(DcMotor.class, "right_drive_one");
         rightDriveTwo = hardwareMap.get(DcMotor.class, "right_drive_two");
 
+        //Initialize the servos.
         neckPosition = hardwareMap.get(Servo.class, "neck_position_servo");
         leftShoulder = hardwareMap.get(Servo.class, "left_shoulder_servo");
         rightShoulder = hardwareMap.get(Servo.class, "right_shoulder_servo");
         leftElbow = hardwareMap.get(Servo.class, "left_elbow_servo");
         rightElbow = hardwareMap.get(Servo.class, "right_elbow_servo");
+
+        //Initialize the sensors.
+        leftColorSensor = hardwareMap.get(ColorSensor.class, "left_color_sensor");
+        rightColorSensor = hardwareMap.get(ColorSensor.class, "right_color_sensor");
+
+        touchSensor = hardwareMap.get(TouchSensor.class, "touch_sensor");
+        distanceSensor = hardwareMap.get(SensorREV2mDistance.class, "distance_sensor");
 
         leftDriveOne.setDirection(DcMotorSimple.Direction.REVERSE);
         rightDriveOne.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -95,6 +120,7 @@ public class KillerRobot extends LinearOpMode {
 
     protected void stopKillerRobot(){
 
+        objectIsClose = true;
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/SimpleRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/SimpleRobot.java
@@ -1,14 +1,29 @@
 package org.firstinspires.ftc.teamcode.killer_robot;
 
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp(name = "SimpleRobot", group = "Linear Opmode")
 public class SimpleRobot extends LinearOpMode {
 
-    DcMotor motor;
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+
+    Servo leftShoulder;
+    Servo rightShoulder;
+
+    ColorSensor colorSensor;
+
+    DigitalChannel touchSensor;
+
+    Rev2mDistanceSensor distanceSensor;
+
     public boolean objectIsClose = false;
     private float POWER = 0.7f;
     private String TAG = "Status: ";
@@ -16,32 +31,69 @@ public class SimpleRobot extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-    motor = hardwareMap.get(DcMotor.class, "drive");
 
-    initializeSensors();
-
-    while(opModeIsActive() && !objectIsClose){
-        telemetry.addData("Status: ", "Starting loop.");
-        telemetry.update();
-        runMotor();
-        stopRobot();
-    }
-
-    }
+        initializeHardware();
+        initializeSensors();
 
 
-    public void runMotor(){
-        motor.setPower(POWER);
-    }
-
-    protected void stopRobot(){
-
+        waitForStart();
+        while (opModeIsActive() && !objectIsClose) {
+            telemetry.addData(TAG, "Opmode is running.");
+            telemetry.update();
+            leftShoulder.setPosition(0.4);
+            rightShoulder.setPosition(0.5);
+            //leftDrive.setPower(POWER);
+            //rightDrive.setPower(POWER);
+            stopRobot();
+            sleep(1000);
+        }
 
     }
 
-    protected void initializeSensors(){
-        telemetry.addData(TAG, "Initilize Sensors called.");
+
+    protected void stopRobot() {
+
+        telemetry.addData(TAG, touchSensor.getState());
         telemetry.update();
 
+        if (touchSensor.getState() == true) {
+            telemetry.addData(TAG, "Touch pressed.");
+            objectIsClose = true;
+            telemetry.update();
+            resetRobot();
+        }
+
     }
+
+    protected void resetRobot() {
+        leftDrive.setPower(0.0);
+        rightDrive.setPower(0.0);
+        leftShoulder.setPosition(0.0);
+        rightShoulder.setPosition(0.0);
+    }
+
+
+    protected void initializeHardware() {
+
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+
+        leftShoulder = hardwareMap.get(Servo.class, "left_shoulder");
+        rightShoulder = hardwareMap.get(Servo.class, "right_shoulder");
+
+        leftShoulder.setPosition(0.0);
+        rightShoulder.setPosition(0.0);
+
+    }
+
+
+    protected void initializeSensors() {
+        touchSensor = hardwareMap.get(DigitalChannel.class, "touch_sensor");
+        touchSensor.setMode(DigitalChannel.Mode.INPUT);
+
+        distanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "distance_sensor");
+        colorSensor = hardwareMap.get(ColorSensor.class, "color_sensor");
+
+    }
+
 }
