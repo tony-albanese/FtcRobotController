4090f3e18632972c51115d651731119f98223ba6
tony-albanese <tony.albanese2000@gmail.com>
2020-12-18 11:41:19 +0100

    Initial commit.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/StopKillerRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/StopKillerRobot.java
@@ -2,11 +2,8 @@ package org.firstinspires.ftc.teamcode.killer_robot;
 
 public class StopKillerRobot extends KillerRobot {
 
-
     @Override
-    public void stopKillerRobot(){
-        sleep(5000);
-        objectIsClose = true;
+    protected void stopKillerRobot() {
+        
     }
-
 }
