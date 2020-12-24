a55b016c65c6fd3b34ee8e6f0060ba44563cf404
tony-albanese <tony.albanese2000@gmail.com>
2020-12-24 15:19:16 +0100

    Cleaned up code to have basic methods to override.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/StopKillerRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/StopKillerRobot.java
@@ -1,9 +1,19 @@
 package org.firstinspires.ftc.teamcode.killer_robot;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Name: ", group = "Stop Killer Robot")
 public class StopKillerRobot extends KillerRobot {
+
 
     @Override
     protected void stopKillerRobot() {
+
+
+    }
+
+    @Override
+    protected void startKillerRobot() {
         
     }
 }
