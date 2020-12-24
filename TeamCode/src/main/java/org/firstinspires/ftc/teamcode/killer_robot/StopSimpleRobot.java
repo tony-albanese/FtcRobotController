daa20052a9606f9efe5be59d9842018d1f58fa91
tony-albanese <tony.albanese2000@gmail.com>
2020-12-24 16:04:02 +0100

    Set up base code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/StopSimpleRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/StopSimpleRobot.java
@@ -1,5 +1,17 @@
 package org.firstinspires.ftc.teamcode.killer_robot;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Stop Simple Robot", group = "Killer Robot Test")
 public class StopSimpleRobot extends SimpleRobot {
 
+    @Override
+    protected void stopKillerRobot() {
+        
+    }
+
+    @Override
+    protected void startKillerRobot() {
+
+    }
 }
