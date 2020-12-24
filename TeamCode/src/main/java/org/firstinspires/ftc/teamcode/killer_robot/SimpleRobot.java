f40da96c7f73b91540047e2f1a641e5b4a42a8e7
tony-albanese <tony.albanese2000@gmail.com>
2020-12-24 15:21:00 +0100

    Removed cluttered test code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/SimpleRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/killer_robot/SimpleRobot.java
@@ -1,99 +1,17 @@
 package org.firstinspires.ftc.teamcode.killer_robot;
 
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name = "SimpleRobot", group = "Linear Opmode")
+@TeleOp(name = "SimpleRobot Base Class", group = "Linear Opmode")
+@Disabled
 public class SimpleRobot extends LinearOpMode {
-
-    DcMotor leftDrive;
-    DcMotor rightDrive;
-
-    Servo leftShoulder;
-    Servo rightShoulder;
-
-    ColorSensor colorSensor;
-
-    DigitalChannel touchSensor;
-
-    Rev2mDistanceSensor distanceSensor;
-
-    public boolean objectIsClose = false;
-    private float POWER = 0.7f;
-    private String TAG = "Status: ";
 
 
     @Override
     public void runOpMode() throws InterruptedException {
-
-        initializeHardware();
-        initializeSensors();
-
-
-        waitForStart();
-        while (opModeIsActive() && !objectIsClose) {
-            telemetry.addData(TAG, "Opmode is running.");
-            telemetry.update();
-            leftShoulder.setPosition(0.4);
-            rightShoulder.setPosition(0.5);
-            //leftDrive.setPower(POWER);
-            //rightDrive.setPower(POWER);
-            stopRobot();
-            sleep(1000);
-        }
-
+        
     }
-
-
-    protected void stopRobot() {
-
-        telemetry.addData(TAG, touchSensor.getState());
-        telemetry.update();
-
-        if (touchSensor.getState() == true) {
-            telemetry.addData(TAG, "Touch pressed.");
-            objectIsClose = true;
-            telemetry.update();
-            resetRobot();
-        }
-
-    }
-
-    protected void resetRobot() {
-        leftDrive.setPower(0.0);
-        rightDrive.setPower(0.0);
-        leftShoulder.setPosition(0.0);
-        rightShoulder.setPosition(0.0);
-    }
-
-
-    protected void initializeHardware() {
-
-        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-
-        leftShoulder = hardwareMap.get(Servo.class, "left_shoulder");
-        rightShoulder = hardwareMap.get(Servo.class, "right_shoulder");
-
-        leftShoulder.setPosition(0.0);
-        rightShoulder.setPosition(0.0);
-
-    }
-
-
-    protected void initializeSensors() {
-        touchSensor = hardwareMap.get(DigitalChannel.class, "touch_sensor");
-        touchSensor.setMode(DigitalChannel.Mode.INPUT);
-
-        distanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "distance_sensor");
-        colorSensor = hardwareMap.get(ColorSensor.class, "color_sensor");
-
-    }
-
 }
